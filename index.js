d51230d7bcfb343f9807774215b77ace7019c9e0
deepankar96 <dipankar.sarmah1639@gmail.com>
2020-02-12 13:43:03 +0530

    Angualar crud

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
+const mysql = require('mysql');
 const express = require('express')
+const bodyparser = require('body-parser');
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.use(bodyparser.json());
 
-app.listen(3000,()=>{
-    console.log("Listening on port 3000")
+var mysqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'college-system',
+  multipleStatements: true
+  });
+
+  mysqlConnection.connect((err)=> {
+    if(!err)
+    console.log('Connection Established Successfully');
+    else
+    console.log('Connection Failed!'+ JSON.stringify(err,undefined,2));
+    });
+
+    //Create Data in the database
+    app.get('/add/:email/:pass/:username',(req,res)=>{
+      var sql = 'INSERT into user (email,password,username) values ?';
+      var value = [req.params.email,req.params.pass,req.params.username];
+      mysqlConnection.query(sql,[value], (err,rows) => {
+      if(err) throw err;
+      res.send(result.affectedRows);
+    });}
+  );
+
+    //Recieve Data from the database
+    app.get('/:id',(req,res)=>{
+      var userId = req.params.id;
+      var sql = 'SELECT * FROM user WHERE Id= ?';
+        mysqlConnection.query(sql,[userId], (err,rows) => {
+        if(err) throw err;
+        res.send(rows);
+      });
+      }
+    );
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}..`);
 })
