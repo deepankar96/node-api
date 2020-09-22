6e91dbce314d02c98f648d9d607b82eaea8b0c15
Deepankar Sarmah <34167524+deepankar96@users.noreply.github.com>
2020-09-22 13:00:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyparser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const collegeauth = require('./middleware/college-auth');
-
+//
 const app = express()
  
 app.use(bodyparser.json());
